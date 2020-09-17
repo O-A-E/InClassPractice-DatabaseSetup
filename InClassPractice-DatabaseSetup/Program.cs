2c7dd7a3cc12f5c18628d00b53ad66f03293e613
O-A-E <67084069+O-A-E@users.noreply.github.com>
2020-09-17 11:34:50 -0600

    Add project files.

---
+++ InClassPractice-DatabaseSetup/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace InClassPractice_DatabaseSetup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* In-Class Practice
+
+           -Perform this database setup in a new project with a "Manufacturer" table.
+
+           -Properties:
+               -ID (id, int(10), primary key, auto increment)
+               -Name (varchar(30), not null)
+               -YearFounded (int(10), null)
+
+            -Seed Data (Null founded date if not specified):
+               -Chevrolet, 1911
+               -Ford, 1903
+               -Tesla
+               -Dodge
+               -Toyota, 1937
+               -Honda, 1946
+               -Mitsubishi
+               -Nissan
+            */
+        }
+    }
+}
